b53acab52ffb82f0ac58a1db244b35d17b26d689
l0kt33n <aa77855@gmail.com>
2023-09-02 17:05:41 -0700

    upgrade and updated configs

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
-# Next.js + Contentlayer
+# My Personal Blog using Next.js, Contentlayer, TailwindCSS and Vercel
 
-A template with Next.js 13 app dir, Contentlayer, Tailwind CSS and dark mode.
+This is my personal blog, built with [Next.js](https://nextjs.org/), [Contentlayer](https://contentlayer.dev/) and [TailwindCSS](https://tailwindcss.com/). It is hosted on [Vercel](https://vercel.com/).
 
-https://next-contentlayer.vercel.app
+## Getting Started
 
+First, install the dependencies:
 
+```bash
+pnpm install
+```
+
+Then, run the development server:
+
+```bash
+pnpm dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+## Writing Content
+
+All content is written in Markdown and lives in the `content` directory. The content is structured in a way that makes sense for me, but you can change it to your liking.
--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,4 +1,8 @@
 import { defineDocumentType, makeSource } from "contentlayer/source-files"
+import rehypePrettyCode from "rehype-pretty-code"
+import remarkGfm from 'remark-gfm'
+import rehypeAutolinkHeadings from "rehype-autolink-headings"
+import rehypeSlug from "rehype-slug"
 
 /** @type {import('contentlayer/source-files').ComputedFields} */
 const computedFields = {
@@ -51,4 +55,38 @@ export const Post = defineDocumentType(() => ({
 export default makeSource({
   contentDirPath: "./content",
   documentTypes: [Post, Page],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypePrettyCode,
+        {
+          theme: "github-dark",
+          onVisitLine(node) {
+            // Prevent lines from collapsing in `display: grid` mode, and allow empty
+            // lines to be copy/pasted
+            if (node.children.length === 0) {
+              node.children = [{ type: "text", value: " " }]
+            }
+          },
+          onVisitHighlightedLine(node) {
+            node.properties.className.push("line--highlighted")
+          },
+          onVisitHighlightedWord(node) {
+            node.properties.className = ["word--highlighted"]
+          },
+        },
+      ],
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ["subheading-anchor"],
+            ariaLabel: "Link to section",
+          },
+        },
+      ],
+    ],
+  },
 })
