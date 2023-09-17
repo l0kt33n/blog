4a161f919274725f5da362f0a57c462398ba1b1d
l0kt33n <aa77855@gmail.com>
2023-09-17 15:57:13 -0700

    open contact in new tab

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Linkedin, Github, Mail, Check, CheckCircle } from 'lucide-react'
+import Link from 'next/link'
 
 export default function Page() {
   return (
@@ -40,15 +41,15 @@ export default function Page() {
           </div>
         </div>
         <div className="flex flex-col items-center mt-16 space-y-8 lg:flex-row lg:items-start lg:space-y-0 lg:space-x-6">
-          <a href="mailto:yulokteen@outlook.com" className="text-gray-400 hover:text-gray-500">
+          <Link target='_blank' href="mailto:yulokteen@outlook.com" className="text-gray-400 hover:text-gray-500">
             <Mail size={24} />
-          </a>
-          <a href="https://www.linkedin.com/lokteenyu" className="text-gray-400 hover:text-gray-500">
+          </Link>
+          <Link target='_blank' href="https://www.linkedin.com/lokteenyu" className="text-gray-400 hover:text-gray-500">
             <Linkedin size={24} />
-          </a>
-          <a href="https://github.com/l0kt33n" className="text-gray-400 hover:text-gray-500">
+          </Link>
+          <Link target='_blank' href="https://github.com/l0kt33n" className="text-gray-400 hover:text-gray-500">
             <Github size={24} />
-          </a>
+          </Link>
         </div>
       </div>
     </div>
